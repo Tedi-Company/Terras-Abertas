7ac23145765ff5ceae9a4ffae74da74c543e8a01
Tedi-Company <lelecanalfahl@gmail.com>
2026-05-16 22:49:14 -0300

    Update water.lua

--- mods/base/blocos/water.lua
+++ mods/base/blocos/water.lua
@@ -1,5 +1,6 @@
+-- 1. REGISTRO DA FONTE DE ÁGUA (BLOCO PARADO)
 minetest.register_node("base:water_source", {
-    description = "Água",
+    description = "Água Source",
     tiles = {"water.png"},
     drawtype = "liquid",
     paramtype = "light",
@@ -12,4 +13,22 @@ minetest.register_node("base:water_source", {
     liquid_alternative_source = "base:water_source",
     liquid_viscosity = 1,
     groups = {water = 3, liquid = 3},
+})
+
+-- 2. REGISTRO DA ÁGUA FLUINDO (O QUE ESCORRE)
+minetest.register_node("base:water_flowing", {
+    description = "Água Flowing",
+    tiles = {"water.png"},
+    drawtype = "flowingliquid",
+    paramtype = "light",
+    paramtype2 = "flowingliquid",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    liquidtype = "flowing",
+    liquid_alternative_flowing = "base:water_flowing",
+    liquid_alternative_source = "base:water_source",
+    liquid_viscosity = 1,
+    groups = {water = 3, liquid = 3, not_in_creative_inventory = 1},
 })
